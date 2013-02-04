eb288355ee072e5546b9802637e1c08dec1e6a4a
Ben Striegel <ben.striegel@gmail.com>
2013-02-04 15:41:35 -0500

    Add reponame as an input parameter

--- get_data.py
+++ get_data.py
@@ -8,9 +8,9 @@ import shutil
 import sys
 import time
 
-# TODO: make repo name an input parameter
+reponame = sys.argv[1]
 
-username = sys.argv[1]
+username = sys.argv[2]
 password = getpass.getpass('Github password for {0}: '.format(username))
 cred = (username, password)
 
@@ -19,7 +19,7 @@ cred = (username, password)
 # to get some representative test data.
 # This is the only way to get additions and deletions for each user for a repo,
 # but it will only return the top 100 users. We get the rest later.
-#r = requests.get('https://github.com/mozilla/rust/graphs/contributors-data')
+#r = requests.get('https://github.com/{0}/graphs/contributors-data'.format(reponame))
 r = requests.get('http://seleniac.org/map/contrib.json')
 topcontributors = r.json()
 print(topcontributors)
@@ -34,12 +34,10 @@ for user in topcontributors:
     topnames.append(name)
     data.append({'name': name, 'impact': impact})
 
-#print(data)
-
 # Here's where we get the names of each user who wasn't in the top 100.
 # Because we have no impact data, we just give them an impact of 1.
 # The web page itself will ensure that their dot is large enough to be visible.
-r = requests.get('https://api.github.com/repos/mozilla/rust/contributors', auth=cred)
+r = requests.get('https://api.github.com/repos/{0}/contributors'.format(reponame), auth=cred)
 contributors = r.json()
 for user in contributors:
     name = user['login']
@@ -48,21 +46,20 @@ for user in contributors:
 
 print(data)
 
+# Now we look up each user in turn on Github, grab their location data,
+# and run it through the Google Maps API.
 for user in data:
     name = user['name']
     print(name)
     r = requests.get('https://api.github.com/users/'+name, auth=cred)
     userdata = r.json()
-    #print(userdata)
     if 'location' in userdata:
         location = userdata['location'] or ''
     else:
         location = ''
 
-    r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address='+location+'&sensor=false',
-                      auth=cred)
+    r = requests.get('http://maps.googleapis.com/maps/api/geocode/json?address='+location+'&sensor=false')
     geodata = r.json()
-    #print(geodata)
     if geodata['status'] != 'OK':
         lat = 'MOON'
         lon = 'MOON'
@@ -81,6 +78,6 @@ date_filename = 'data/' + datetime.datetime.now().strftime('%Y%m%d_%H%M') + '.js
 with open(date_filename, 'w') as f:
     f.write(json.dumps(data))
 
-shutil.copyfile(date_filename, 'data/newest.json')
+shutil.copyfile(date_filename, 'data/newest.json')  # Destructive copy
 
 print('Complete')
